385d2005ce382daa080b47b30507a290a12886b3
Danielle Scheele <strangeyellowbird@gmail.com>
2016-05-17 14:12:50 -0500

    update gh-pages

---
+++ module2.js
@@ -0,0 +1,51 @@
+var chatty = (function(chatty) {
+	//This iife contains all methods corresponding with getting and setting
+	//information in the private array.
+	var messages = [];
+	//Loops through private array and prints each message to the DOM.
+	//Also sets the elements id
+	chatty.setMessageInDOM = function(userInput, messageDestination) {
+		var HTML = "";
+		for (var i = 0; i < messages.length; i++) {
+			HTML += `<div id="msg-${messages[i].msgid}" class="messages">`;
+			HTML += `<span>${messages[i].message}</span><button id="${messages[i].msgid}" class="buttons">Delete</button></div>`
+		};
+		messageDestination.innerHTML = HTML;
+	};
+
+	//Adds new message to the private array
+	chatty.setPrivateArray = function(userInput) {
+		var newUserInputObject = {
+			"msgid": (messages.length + 1),
+			"message": userInput
+		}
+		messages.push(newUserInputObject);
+	};
+
+	//Adds the JSON data to the private array
+	chatty.setJSONInArray = function(JSONData) {
+		for (var i = 0; i < JSONData.length; i++) {
+			messages.push(JSONData[i]);
+			chatty.setMessageInDOM(messages, messageBoard);
+		};
+	};
+
+	//When delete button is pressed, this function deletes the corresponding
+	//message from the private array.
+	chatty.deleteFromPrivateArray = function(id) {
+		messages.splice(messages[id - 1], 1);
+	};
+
+	//Deletes all messages from the private array
+	chatty.deleteAllFromPrivateArray = function() {
+		messages.splice(0, messages.length);
+	}
+
+	//This makes the content of the private array available to view but not change
+	chatty.getPrivateArray = function() {
+		return messages
+	};
+
+	return chatty;
+
+})(chatty || {});
---
+++ module3.js
@@ -0,0 +1,20 @@
+var chatty = (function(chatty){
+
+  //Deletes the child and parent of the event.target
+  chatty.deleteDomElement = function(DOMelement, targetId) {
+    DOMelement.parentNode.parentNode.removeChild(DOMelement.parentNode);
+    //calls function to remove corresponding item from private array
+    chatty.deleteFromPrivateArray(targetId);
+    //checks if message board is empty. If yes then it disables clearAll button
+    checkMessage();
+  };
+
+  //Deletes the entire message board from the DOM
+  chatty.deleteAll = function(clearAllId){
+    messageBoard.innerHTML = " ";
+  };
+
+  return chatty;
+
+})(chatty || {});
+
